02b8ac0df002e813dee4d506798b81f059c6751f
Andrei Quirante <andrei.quirante@ciit.edu.ph>
2024-11-23 04:26:21 +0800

    yes

--- Assets/Scripts/Enemy/RandomizePosition.cs
+++ Assets/Scripts/Enemy/RandomizePosition.cs
@@ -12,7 +12,7 @@ public class RandomizePosition : MonoBehaviour
         for (int i = 0; i <= numberOfSpawns; i++)
         {
             int randomIndex = Random.Range(0, enemyGO.Length);
-            Vector3 randomSpawn = new Vector3(Random.Range(-range, range), 2, Random.Range(-range, range));
+            Vector3 randomSpawn = new Vector3(transform.position.x + Random.Range(-range, range), 2, transform.position.z + Random.Range(-range, range));
             var enemySpawn = Instantiate(enemyGO[randomIndex], randomSpawn, Quaternion.identity);
             enemySpawn.transform.parent = enemyParent.transform;
         }
